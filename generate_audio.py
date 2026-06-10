1160875e693e8678a4a334db8d0051e67eefffa8
mczyo <menardagodo@gmail.com>
2026-06-10 13:15:40 +0800

    finished last letter

--- generate_audio.py
+++ generate_audio.py
@@ -1,9 +1,9 @@
 from gtts import gTTS
 
 # Tricking the TTS engine with phonetic spelling to force a humming sound
-text_to_read = "Hari... Halaman... Hangin"
+text_to_read = "Wa-lis... Walo... Watawat"
 
 # Generate the audio
 tts = gTTS(text=text_to_read, lang='tl')
-tts.save("h_pronounce.mp3")
+tts.save("w_pronounce.mp3")
 print("Audio generated! Go listen to generated audio")
